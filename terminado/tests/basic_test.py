1c322b7f124dde23097a91b802860390b707f5f0
Andrew Payne <andy@payne.org>
2015-02-03 10:34:33 -0500

    Refactoring:  generalize connection to test terminal into TestTermManager object

--- terminado/tests/basic_test.py
+++ terminado/tests/basic_test.py
@@ -13,6 +13,44 @@ import tornado.testing
 import logging
 import json
 
+class TestTermManager(object):
+    """Test connection to a terminal manager"""
+    def __init__(self, path, test_case):
+        """Open a Websocket connection to the localhost"""
+        port = test_case.get_http_port()
+        url = 'ws://127.0.0.1:%d%s' % (port, path)
+        request = tornado.httpclient.HTTPRequest(url, 
+                    headers={'Origin' : 'http://127.0.0.1:%d' % port})
+        self.ws_future = tornado.websocket.websocket_connect(request)
+        self.stdout = ""
+
+    @tornado.gen.coroutine
+    def read_msg(self):
+        ws = yield self.ws_future
+        response = yield ws.read_message()
+        msg = json.loads(response)
+
+        # If stdout output, save it
+        if msg[0] == 'stdout':
+            self.stdout += msg[1]
+        raise tornado.gen.Return(msg)
+
+    @tornado.gen.coroutine
+    def write_msg(self, msg):
+        ws = yield self.ws_future
+        ws.write_message(json.dumps(msg))
+
+    @tornado.gen.coroutine
+    def write_stdin(self, data):
+        """Write to terminal's stdin"""
+        self.write_msg(['stdin', data])
+
+    @tornado.gen.coroutine
+    def close(self):
+        ws = yield self.ws_future
+        ws.close()
+
+
 class BasicTest(tornado.testing.AsyncHTTPTestCase):
     def get_app(self):
         named_tm = NamedTermManager(shell_command=['bash'], ioloop=self.io_loop)
@@ -26,38 +64,24 @@ class BasicTest(tornado.testing.AsyncHTTPTestCase):
 
     test_urls = ('/named/term1', '/single', '/unique')
 
-    @tornado.gen.coroutine
-    def ws_connect(self, path):
-        """Open a Websocket connection to the localhost"""
-        port = self.get_http_port()
-        url = 'ws://127.0.0.1:%d%s' % (port, path)
-        request = tornado.httpclient.HTTPRequest(url, 
-                    headers={'Origin' : 'http://127.0.0.1:%d' % port})
-        ws = yield tornado.websocket.websocket_connect(request)
-        raise tornado.gen.Return(ws)
-
-    @tornado.gen.coroutine
-    def read_json(self, ws):
-        """Helper:  read a message, parse as JSON"""
-        response = yield ws.read_message()
-        raise tornado.gen.Return(json.loads(response))
-
     @tornado.testing.gen_test
     def test_basic(self):
         for url in self.test_urls:
-            ws = yield self.ws_connect(url)
-            response = yield self.read_json(ws)             # Setup message
+            tm = TestTermManager(url, self)
+            response = yield tm.read_msg()                    
             self.assertEqual(response, ['setup', {}])
 
-            response = yield self.read_json(ws)             # Command prompt
+            # Check for initial shell prompt
+            response = yield tm.read_msg()
             self.assertEqual(response[0], 'stdout')
             self.assertGreater(len(response[1]), 0)
-            ws.close()
+            tm.close()
 
     @tornado.testing.gen_test
     def test_named_no_name(self):
         with self.assertRaises(HTTPError) as context:
-            ws = yield self.ws_connect('/named/')
+            tm = TestTermManager('/named/', self)
+            yield tm.read_msg()
 
         # Not found
         self.assertEqual(context.exception.code, 404)
