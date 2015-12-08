96073f1722ccae608a3d4ac93a6e018d42197d66
root <root@data301a.calpolydatascience.org>
2015-12-08 21:06:24 +0000

    Temp fix for EBADF

--- terminado/management.py
+++ terminado/management.py
@@ -168,8 +168,7 @@ class TermManagerBase(object):
         self.ioloop.add_handler(fd, self.pty_read, self.ioloop.READ)
 
     def on_eof(self, ptywclients):
-        """Called when the pty has closed.
-        """
+        """Called when the pty has closed."""
         # Stop trying to read from that terminal
         fd = ptywclients.ptyproc.fd
         self.log.info("EOF on FD %d; stopping reading", fd)
@@ -180,6 +179,13 @@ class TermManagerBase(object):
         # This should reap the child process
         ptywclients.ptyproc.isalive()
 
+    def on_oserror(self, ptywclients):
+        """Called when there is a EBADF (errno=9) from stopping."""
+        fd = ptywclients.ptyproc.fd
+        self.log.info("EOF on FD %d; stopping reading", fd)
+        del self.ptys_by_fd[fd]
+        self.ioloop.remove_handler(fd)
+        
     def pty_read(self, fd, events=None):
         """Called by the event loop when there is pty data ready to read."""
         ptywclients = self.ptys_by_fd[fd]
@@ -192,6 +198,8 @@ class TermManagerBase(object):
             self.on_eof(ptywclients)
             for client in ptywclients.clients:
                 client.on_pty_died()
+        except OSError:
+            self.on_oserror(ptywclients)
 
     def get_terminal(self, url_component=None):
         """Override in a subclass to give a terminal to a new websocket connection
@@ -210,7 +218,7 @@ class TermManagerBase(object):
     @gen.coroutine
     def shutdown(self):
         yield self.kill_all()
-
+        
     @gen.coroutine
     def kill_all(self):
         futures = []
@@ -322,7 +330,13 @@ class NamedTermManager(TermManagerBase):
         name = ptywclients.term_name
         self.log.info("Terminal %s closed", name)
         self.terminals.pop(name, None)
-    
+
+    def on_oserror(self, ptywclients):
+        super(NamedTermManager, self).on_oserror(ptywclients)
+        name = ptywclients.term_name
+        self.log.info("Terminal %s closed", name)
+        self.terminals.pop(name, None)
+
     @gen.coroutine
     def kill_all(self):
         yield super(NamedTermManager, self).kill_all()
