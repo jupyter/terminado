fd48d51fab0c3815336ff63545a11955b9fcc975
Mike Dearman <5084545+devmonkey22@users.noreply.github.com>
2019-06-10 15:21:50 -0400

    Clean up classifiers based on #57, and remove unneeded setup.py deps (since #65 isnt merged)

--- setup.py
+++ setup.py
@@ -11,11 +11,8 @@ setuptools.setup(
     classifiers=[
         "Environment :: Web Environment",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Topic :: Terminals :: Terminal Emulators/X Terminals",
     ],
     license="MIT",
@@ -23,7 +20,5 @@ setuptools.setup(
         "ptyprocess;os_name!='nt'",
         "pywinpty (>=0.5);os_name=='nt'",
         "tornado (>=4)",
-        "python-interface",
-        "msgpack"
     ]
 )
