f90d6f483ede1fdef657738c65acfcc8eefef63c
Steven Silvester <ssilvester@apple.com>
2021-08-12 06:09:28 -0500

    Release 0.11.0

--- terminado/__init__.py
+++ terminado/__init__.py
@@ -12,4 +12,4 @@ import logging
 # Prevent a warning about no attached handlers in Python 2
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '0.10.1'
+__version__ = '0.11.0'
